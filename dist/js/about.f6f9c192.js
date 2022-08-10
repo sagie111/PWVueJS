8023b8067e567bdc94663533c64b8e1625db9b91
sagie111 <71749018+sagie111@users.noreply.github.com>
2022-08-10 16:30:40 +0300

    Add files via upload

---
+++ dist/js/about.f6f9c192.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvuetify_first_app"]=self["webpackChunkvuetify_first_app"]||[]).push([[443],{2741:function(l,n,e){e.r(n),e.d(n,{default:function(){return h}});var u=function(){var l=this,n=l._self._c;return n("hello-world")},t=[],r=function(){var l=this,n=l._self._c;return n("div")},s=[],f=e(1001),o={},i=(0,f.Z)(o,r,s,!1,null,null,null),c=i.exports,p={name:"Home",components:{HelloWorld:c}},a=p,_=(0,f.Z)(a,u,t,!1,null,null,null),h=_.exports}}]);
+//# sourceMappingURL=about.f6f9c192.js.map
