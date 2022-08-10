6abe031956beda5885afcbb353e5006a77334e70
sagie111 <71749018+sagie111@users.noreply.github.com>
2022-08-10 16:42:26 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app id="inspire">    
+    <v-navigation-drawer  
+    v-model="drawer"
+    app
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6 font-weight-bold">
+            Sagie Dvir
+          </v-list-item-title>
+          <v-list-item-subtitle class="font-weight-bold">
+            Full Stack Developer
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title style="color: black; font-weight: bold;">Sagie Dvir Portfolio ☕</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+
+        <router-view></router-view>
+      
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      
+        items: [
+          { title: 'About', icon: 'mdi-card-account-details', to: '/' },
+          { title: 'Projects', icon: 'mdi-image', to: '/about' },
+          //{ title: 'About', icon: 'mdi-help-box' },
+        ],
+      }),
+  }
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+
+
+
+<template>
+
+  <div class="about pa-6 text-center">
+    
+        <h1 class="purple--text display-2 font-weight-bold mb-3" style="margin-top: 30px;">
+          Who am i?
+        </h1>
+    <p class="font-weight-bold" style="margin-top: 30px;">Hi, I am Sagie. I'm 16 years old <code class="font-weight-bold purple--text">Full Stack Developer</code>. I learned all what I know by myself. I'm trying to improve my skills every single day. Enjoy my projects😊</p>
+      <hr>
+
+
+    <h1 class="purple--text display-2 font-weight-bold mb-3" style="margin-top: 30px;">Tools I use</h1>
+
+
+    
+        <h2 style="margin-top: 30px; text-decoration: underline;">Back-end:</h2>
+
+        <div style="display: block;">
+          <h2 style="margin-top: 10px;">Python 🐍</h2>
+          <h2>Java ☕</h2>
+        </div>
+      <h2 style="margin-top: 30px; text-decoration: underline;">Front-end:</h2>
+          <h2 style="margin-top: 10px;">HTML 📜</h2>
+          <h2>CSS 🍯</h2>
+          <H2>JS 💛</H2>
+
+      <h2 style="margin-top: 30px; text-decoration: underline;">frameworks:</h2>
+      <h2 style="margin-top: 10px;">VueJS 🌻</h2>
+
+
+      
+
+    </div>
+</template>
+
